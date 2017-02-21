e9b1012a9190b8a0f272b697c15cfa8810025bc2
Scott Haefner <shaefner@usgs.gov>
2017-02-21 14:33:29 -0800

    add html; fix refs; add global vars

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -7,11 +7,15 @@ if (!isset($TEMPLATE)) {
   $NAVIGATION = true;
   $HEAD = '
     <link rel="stylesheet" href="/lib/leaflet-0.7.7/leaflet.css" />
-    <link rel="stylesheet" href="css/index.css"/>
+    <link rel="stylesheet" href="helicorders/css/index.css" />
   ';
   $FOOT = '
+    <script>
+      var MOUNT_PATH = "' . $CONFIG['MOUNT_PATH'] . '",
+          SET = "nca";
+    </script>
     <script src="/lib/leaflet-0.7.7/leaflet.js"></script>
-    <script src="js/index.js"></script>
+    <script src="helicorders/js/index.js"></script>
   ';
 
 
@@ -20,4 +24,11 @@ if (!isset($TEMPLATE)) {
 
 ?>
 
+<p>These seismogram displays depict ground motion recorded by seismograph
+  stations in real-time, updated every few minutes. Each plot represents 24
+  hours of data from one station. <a href="/monitoring/helicorders/about.php">Read
+  more</a> &raquo;</p>
+
 <div class="map"></div>
+
+<h3 class="count"></h3>

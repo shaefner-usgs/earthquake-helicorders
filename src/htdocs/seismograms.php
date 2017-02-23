b8f8ca49f3c7df6c36d1aa249ce5f7015de85780
Scott Haefner <shaefner@usgs.gov>
2017-02-23 15:48:47 -0800

    add option to view plots from all stations at once

--- src/htdocs/seismograms.php
+++ src/htdocs/seismograms.php
@@ -8,6 +8,7 @@ include_once '../lib/classes/Db.class.php'; // db connector, queries
 
 $db = new Db;
 
+$date = safeParam('date');
 $id = safeParam('id');
 
 // 'hardwire' for now
@@ -19,57 +20,81 @@ if (!isset($TEMPLATE)) {
   $HEAD = '<link rel="stylesheet" href="css/seismograms.css" />';
   $FOOT = '';
 
+  function getImg ($date, $id, $instrument) {
+    $imgDateStr = $date;
+    if ($date === 'latest') {
+      $imgDateStr = '22221212'; // 'latest' plots use this date string
+    }
+
+    $file = sprintf('%s/tn-nc.%s_00.%s00.gif',
+      $GLOBALS['set'],
+      str_replace(' ', '_', $instrument),
+      $imgDateStr
+    );
+
+    // If no image exists, display 'no data' msg
+    if (file_exists($GLOBALS['CONFIG']['DATA_DIR'] . '/' . $file)) {
+      $img = sprintf('<a href="%d/%s">
+          <img src="data/%s" alt="seismogram thumbnail" />
+        </a>',
+        $id,
+        $date,
+        $file
+      );
+    } else {
+      $img = '<p>No data available</p>';
+    }
+
+    return $img;
+  }
+
   include 'template.inc.php';
-}
-
-// Query db to get station details
-$rsStation = $db->queryStation($id);
-
-// If station found, set subtitle; otherwise, show error
-$row = $rsStation->fetch(PDO::FETCH_ASSOC);
-if ($row) {
-  $instrument = $row['site'] . ' ' . $row['type'] . ' ' . $row['network'] .
-    ' ' . $row['code'];
-  $subtitle = $instrument . ' (' . trim($row['name']) . ')';
-} else {
-  print '<p class="alert info">Station Not Found</p>';
-  return;
 }
 
 $listHtml = '<ul class="stations no-style">';
 
-// loop thru past 15 days, plus latest
-for ($i = -1; $i < 15; $i ++) {
-  if ($i === -1) { // latest
-    $date = '22221212';
-    $imgTitle = 'Past 24 hours';
-    $path = 'latest';
+if ($id) { // show plots for a given station
+  // Query db to get station details
+  $rsStation = $db->queryStation($id);
+
+  // If station found, set subtitle; otherwise, show error
+  $row = $rsStation->fetch(PDO::FETCH_ASSOC);
+  if ($row) {
+    $instrument = $row['site'] . ' ' . $row['type'] . ' ' . $row['network'] .
+      ' ' . $row['code'];
+    $subtitle = $instrument . ' (' . trim($row['name']) . ')';
   } else {
-    $date = date('Ymd', strtotime('-' . $i . ' day'));
-    $imgTitle = date('M j, Y', strtotime($date));
-    $path = $date;
+    print '<p class="alert info">Station not found</p>';
+    return;
   }
 
-  $file = sprintf('%s/tn-nc.%s_00.%s00.gif',
-    $set,
-    str_replace(' ', '_', $instrument),
-    $date
-  );
+  // Loop thru past 15 days, plus latest
+  for ($i = -1; $i < 15; $i ++) {
+    if ($i === -1) { // latest
+      $date = 'latest';
+      $imgTitle = 'Past 24 hours';
+    } else {
+      $date = date('Ymd', strtotime('-' . $i . ' day'));
+      $imgTitle = date('M j, Y', strtotime($date));
+    }
+    $img = getImg($date, $id, $instrument);
 
-  // if no image, display 'no data' msg
-  if (file_exists($CONFIG['DATA_DIR'] . '/' . $file)) {
-    $img = sprintf('<a href="%d/%s">
-        <img src="data/%s" alt="seismogram thumbnail" />
-      </a>',
-      $id,
-      $path,
-      $file
-    );
-  } else {
-    $img = '<p>No data available</p>';
+    $listHtml .= "<li><h3>$imgTitle</h3>$img</li>";
   }
+} else { // show plots for all stations on a given date
+  $subtitle = 'All Stations';
+  $header = getHeaderComponents($date);
 
-  $listHtml .= "<li><h3>$imgTitle</h3>$img</li>";
+  // Query db to get a list of stations
+  $rsStations = $db->queryStations();
+
+  while ($row = $rsStations->fetch(PDO::FETCH_ASSOC)) {
+    $instrument = $row['site'] . ' ' . $row['type'] . ' ' . $row['network'] .
+      ' ' . $row['code'];
+    $img = getImg($date, $row['id'], $instrument);
+
+    $listHtml .= "<li><h3>$instrument</h3>$img</li>";
+  }
 }
 
 $listHtml .= '</ul>';
@@ -78,6 +103,16 @@ $listHtml .= '</ul>';
 
 <h2><?php print $subtitle; ?></h2>
 
+<?php if ($header) { ?>
+  <header>
+    <h3><?php print $header['title']; ?></h3>
+    <ul class="no-style">
+      <li><?php print $header['prevLink']; ?></li>
+      <li><?php print $header['nextLink']; ?></li>
+    </ul>
+  </header>
+<?php } ?>
+
 <?php print $listHtml; ?>
 
-<p class="back">&laquo; <a href="../helicorders">Back to all stations</a></p>
+<p class="back">&laquo; <a href="../helicorders">Back to station list / map</a></p>
